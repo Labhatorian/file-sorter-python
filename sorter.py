62f0232d24dd66e94773c22ee29f0c22b51aab3c
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2020-06-23 22:09:38 +0200

    Bugfix and rework logging
    Fixed similarity function also counting in uppercase letters.
    
    Working on a new logging system also for another project

--- sorter.py
+++ sorter.py
@@ -5,7 +5,6 @@ import os, os.path
 import shutil
 import pyfiglet
 from difflib import SequenceMatcher
-import logging
 
 #For getch both Win32 and Unix.
 try:
@@ -23,10 +22,6 @@ except ImportError:
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old)
 
-#log
-log_name = "log.log"
-logging.basicConfig(filename=log_name, filemode='w', format='%(asctime)s - %(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.DEBUG)
-logging.info('Logging started')
 
 #Ascii banners for being cool
 ascii_banner = pyfiglet.figlet_format("Hos File Sorter")
@@ -40,10 +35,10 @@ print(ascii_banner)
 dir_path = os.path.dirname(os.path.realpath(__file__))
 path_script = os.path.realpath(__file__)
 name_script = os.path.basename(__file__)
+log_name = "moved.txt"
 
 #Set percentages and making sure if you want to continue
 print("You are currently in {}".format(dir_path))
-logging.debug("You are currently in {}".format(dir_path))
 
 percentageDec = input("\nPlease enter the similarity percentage. (Default is 60%)")
 try:
@@ -71,22 +66,20 @@ while True:
 if key == "n":
     exit()
 
-logging.info("Percentage set as {} %.".format(percentageDec))
 
 #Start discovering files
+#f = open("moved.txt","w+")
 print("\nDiscovering files in the directory...")
-logging.info("Discovering files in the directory...")
 
 discoveredResults = [name for name in os.listdir('.') if os.path.isfile(name)]
 discoveredResults.remove(name_script)
-discoveredResults.remove(log_name)
+#discoveredResults.remove("moved.txt")
 amountofResults = len(discoveredResults) - 1
 
 #List the files
 print("There are {} files in the chosen directory:".format(len(discoveredResults)))
 for x in discoveredResults:
   print(x)
-logging.info("There are {} files in the chosen directory:".format(len(discoveredResults)))
 
 #Check for similarity between 2 variables funciton
 def similar(a, b):
@@ -94,19 +87,19 @@ def similar(a, b):
 
 #Time for the main part of the script
 print("\nMoving files and creating folders...")
-logging.info("Moving files and creating folders...")
 
 while (amountofResults >= 0):
     #Set up some variables for current file that's being worked on
     print("\nWorking with file {}...".format(discoveredResults[amountofResults]))
     filename, file_extension = os.path.splitext(discoveredResults[amountofResults])
-    logging.info("Working with file {}...".format(discoveredResults[amountofResults]))
+    filename = filename.lower()
 
-    print("Discovering current folders...")
+    #f.write(filename)
+
+    print("Discovering current folders.")
     folderList = [f.path for f in os.scandir(dir_path) if f.is_dir()]
+    folderList = [x.lower() for x in folderList]
     folderListCount = len(folderList) - 1
-    logging.info("Discovering current folders...")
-
     # for x in folderList:
     #     print(x)
 
@@ -114,11 +107,11 @@ while (amountofResults >= 0):
     try:
         while folderListCount >= 0:
             folderpath, foldername = os.path.split(folderList[folderListCount])
+            foldername = foldername.lower()
             similarity = similar(foldername, filename)
             #Check for similarity in filename and foldername, put them together
             if (similarity >= percentage):
                 print("Similarity >= {}%, moving file {} to folder {}. ".format(percentageDec, filename, foldername))
-                logging.info("Similarity >= {}%, moving file {} to folder {}. ".format(percentageDec, filename, foldername))
                 folderLocationExisting = dir_path + "/" + foldername
                 shutil.move(discoveredResults[amountofResults], folderLocationExisting)
                 amountofResults -= 1
@@ -131,27 +124,53 @@ while (amountofResults >= 0):
         if (folderListCount < 0):
             os.mkdir(filename)
             print("Folder created and now moving {}. ".format(filename))
-            logging.info("Folder created and now moving {}. ".format(filename))
             folderLocation = dir_path + "/" + filename
             shutil.move(discoveredResults[amountofResults], folderLocation)
             amountofResults -= 1
             continue
 
     except shutil.Error as e:
-        print("Failed to move current file to folder. Maybe the file already exists in the folder? Skipping...")
-        logging.error("Failed to move current file to folder. Maybe the file already exists in the folder? Skipping...")
+        print("Failed to move current file to folder.")
+        print("File might already exist in folder, skipping...")
         amountofResults -= 1
-    except:
+  # except:
         #fail message feat. James May
-        logging.critical("Failed moving files and creating folders. Script stopped.")
-        print("\n", ascii_banner_fail)
-        print("Something went wrong with moving the files and creating folders.")
-        print("Press any key to quit")
-        getch()
-        exit()
+      #  print("\n", ascii_banner_fail)
+       # print("Something went wrong with moving the files and creating folders.")
+       # print("Press any key to quit")
+       # getch()
+       # exit()
 
-logging.info("Sorting finished")
+# f.close
+
+# if os.path.exists("movedlists"):
+#     try:
+#         folderLocation = "movedlists" + "/" + "moved.txt"
+#         shutil.move("moved.txt", "movedlists")
+#     except shutil.Error as e:
+#         try: 
+#             for i in log_name:
+#                 try:
+#                     os.rename(r"log_name",r'moved.txt%s.xml' % i)
+#                     shutil.move("moved.txt", "movedlists")
+# else:
+#     try:
+#         os.mkdir("movedlists")
+#         shutil.move("moved.txt", "movedlists")
+#     except shutil.Error as e:
+#         try: 
+#             os.rename(r'file path\OLD file name.file type',r'file path\NEW file name.file type')
+#             shutil.move("moved.txt", "movedlists")
+#         except shutil.Error as e:
+#         #fail message feat. James May
+#             print("\n", ascii_banner_fail)
+#             print("Something went wrong with moving the log.")
+#             print("Press any key to quit")
+#             getch()
+#             exit()
+    
+
 print("\n", ascii_banner_success)
-print("Finished with sorting. Check the log for the... log. Save the log if needed because the script will overwrite it next time.")
+print("Finished with sorting. Scroll up for log.")
 print("Press any key to quit")
 getch()
